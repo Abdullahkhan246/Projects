4da460c948a1c4afb14401f541f44d863bcae9f0
Abdullahkhan246 <anittawilson50@gmail.com>
2025-08-30 13:00:00 +0500

    sigma projects

---
+++ sigma projects/javascript/javascript.js
@@ -0,0 +1,10 @@
+let number=30;
+if(number%2===0){
+    console.log("divisible by 2");
+}
+if( number % 3===0){
+    console.log("your number is divisible on 3");
+}
+else{
+    console.log("number is not divisible")
+}
